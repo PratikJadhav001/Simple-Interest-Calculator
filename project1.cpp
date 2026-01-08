b85e858d5ba9d21b77d4b0bfde8b1abd437c062e
Pratik Jadhav <pratikaj5427@gmail.com>
2026-01-08 13:46:33 +0530

    second

--- project1.cpp
+++ project1.cpp
@@ -32,4 +32,38 @@ cin >> choice;
 cout << "\nThank you for using Simple Interest Calculator!" << endl;
 
 return 0;
+} #include <iostream>
+using namespace std;
+
+void clearScreen(){
+system("cls");
+}
+// Simple Interest Calculator
+int main(){
+clearScreen();
+
+float principle,rate,time,interest;
+string choice;
+
+do{
+cout<<"*------Simple Interest Calculator------*"<<endl;
+cout<<"Enter the principle: ";
+cin>>principle;
+cout<<"Enter the rate: ";
+cin>>rate;
+cout<<"Enter the time: ";
+cin>>time;
+
+//Simple Interest Formula Applied
+interest = (principle*rate*time)/100;
+cout<<"The Simple Interest is : "<<interest<<endl;
+
+cout << "\nDo you want to calculate again? (yes/no): ";
+cin >> choice;
+
+}while(choice == "yes" || choice == "YES" || choice == "Yes");
+
+cout << "\nThank you for using Simple Interest Calculator!" << endl;
+
+return 0;
 }
